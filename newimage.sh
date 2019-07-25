2055431d8b3a77b945e2fa2cacd10953c1e5cab3
stuts <stu.j.franks@gmail.com>
2019-07-25 16:15:15 +0100

    Missing slash

--- newimage.sh
+++ newimage.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $DIR/config.sh
 
 IMAGENAME=$1
-IMAGE=${IMAGEBASE}${IMAGENAME}
+IMAGE=${IMAGEBASE}/${IMAGENAME}
 
 READONLYROOT=1
 FLIGHTINSTALL=0
